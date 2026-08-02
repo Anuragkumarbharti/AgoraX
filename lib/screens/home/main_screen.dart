ecedca923a721d5c29bb839e84985d304c832509
Anurag Bharti <SUNNY885314@GMAIL.COM>
2026-08-02 23:05:21 +0530

    Fix missing GoogleFonts import in main_screen.dart

--- lib/screens/home/main_screen.dart
+++ lib/screens/home/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:get/get.dart';
 import 'package:creania/core/theme.dart';
 import '../../services/chat_controller.dart';
